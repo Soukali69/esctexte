70822a0cb8021d231db183d24f9ab2842f6d04e7
Soukali69 <122987593+Soukali69@users.noreply.github.com>
2023-01-18 14:19:38 +0100

    Add files via upload

---
+++ escape_text.lua
@@ -0,0 +1,31 @@
+Citizen.CreateThread(function()
+    while true do
+        AddTextEntry('PM_SCR_MAP', '~p~Carte de Los Santos')
+        AddTextEntry('PM_SCR_GAM', '~p~Prendre l\'avion')
+        AddTextEntry('PM_SCR_INF', '~p~Logs')
+        AddTextEntry('PM_SCR_SET', '~p~Configuration')
+        AddTextEntry('PM_SCR_STA', '~p~Statistiques')
+        AddTextEntry('PM_SCR_GAL', '~p~Galerie')
+        AddTextEntry('PM_SCR_RPL', '~p~Éditeur ∑')
+        AddTextEntry('PM_PANE_CFX', '~p~4You RP')
+        AddTextEntry('FE_THDR_GTAO', "~p~[By]~w~Soukali ~m~| ~p~Soᥙkᥲᥣι#7962 ~m~| ~p~ID : ~w~".. GetPlayerServerId(PlayerId()) .." ~m~| ~w~"..#GetActivePlayers().. "~w~/~p~64 ~w~joueurs ~p~en ligne")
+        AddTextEntry('PM_PANE_LEAVE', '~p~Se déconnecter de 4you');
+        AddTextEntry('PM_PANE_QUIT', '~p~Quitter FiveM');
+        Citizen.Wait(5000)
+    end
+end)
+
+
+-- Other color --
+
+-- ~r~ = red
+-- ~b~ = Blue
+-- ~g~ = Green
+-- ~y~ = Yellow
+-- ~p~ = purple
+-- ~m~ = Grey
+-- ~u~ = Black
+
+
+
+-- Soᥙkᥲᥣι#7962
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+
+fx_version 'adamant'
+
+game 'gta5'
+
+client_scripts {
+
+    'escape_text.lua'
+}
+
+
+-- Soᥙkᥲᥣι#7962
